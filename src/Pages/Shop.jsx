6ba7700651a431d421f1eddb92379c085e7c3ba9
overworker13 <oversmile13@gmail.com>
2025-04-02 08:58:49 +0500

    Added item and popular component

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Component/Hero/Hero";
+import Popular from "../Component/Popular/Popular";
 
 const Shop = () =>{
     return(
         <div>
             <Hero/>
+            <Popular/>
         </div>
     )
 }
